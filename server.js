f3db85621e59e2285e43e76fd472a0ee8466fa54
Daniel Cruz <dcruz.js@gmail.com>
2021-11-29 12:33:30 -0600

    cors allowed

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
-// const helmet = require("helmet");
-// const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 // const bodyParser = require("body-parser");
 const TaskRouter = require("./server/routes/TaskRouter");
 
@@ -10,16 +10,16 @@ require("./server/config/database");
 require("dotenv").config();
 
 // adding Helmet to enhance your API's security
-// app.use(helmet());
+app.use(helmet());
 
 // using bodyParser to parse JSON bodies into JS objects
 // app.use(bodyParser.json());
 
 // enabling CORS for all requests
-// app.use(cors());
+app.use(cors());
 
 // adding morgan to log HTTP requests
-// app.use(morgan("combined"));
+app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
